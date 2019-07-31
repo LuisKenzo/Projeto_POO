2d0a3d935eae8ee3c3eb990c13e252985c5192ee
LuisKenzo <Luis5401>
2019-07-31 13:00:27 -0300

    Testando github com programas do eclipse

---
+++ src/pacote1/objeto1.java
@@ -0,0 +1,15 @@
+package pacote1;
+
+public class objeto1 {
+	public String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+}
---
+++ src/principal.java
@@ -0,0 +1,11 @@
+import pacote1.*;
+public class principal {
+
+	public static void main(String[] args) {
+		System.out.println("Famoso Hello World");
+		objeto1 luis = new objeto1();
+		luis.setNome("Luis");
+		System.out.println(luis.getNome());
+	}
+
+}
